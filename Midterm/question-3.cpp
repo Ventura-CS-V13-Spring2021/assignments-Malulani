2bbcce9f6bdd6de1ed00789f42601af1d48b3f35
Malulani Tucker <>
2021-04-20 03:42:48 +0000

    Finding the max and min and calculating the difference. If the difference is less than 3 it will stop.

--- Midterm/question-3.cpp
+++ Midterm/question-3.cpp
@@ -18,3 +18,17 @@ while (flag == true)
 num1 = getRdNumber();
 num2 = getRdNumber();
 num3 = getRdNumber();
+
+max = findMax(num1, num2, num3);
+min = findMin(num1, num2, num3);
+
+diff = max - min;
+cout << "Num 01: " << num1 << "\tNum 02: " << num2 << "\tNum 03: " << num3 << "\tMax: " << max << "\tMin: " << min << "\tDifference: " << diff << endl;
+
+if (diff > 3)
+writeFile(diff);
+else
+{
+flag = false;
+return 0;
+}
