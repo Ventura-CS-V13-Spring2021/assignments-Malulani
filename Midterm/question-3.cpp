5bba72a08fabe5648f094247786df16c219d0d86
Malulani Tucker <>
2021-04-20 03:50:56 +0000

    Generates a random number between 0 and 9 and gets the min and max

--- Midterm/question-3.cpp
+++ Midterm/question-3.cpp
@@ -32,3 +32,35 @@ else
 flag = false;
 return 0;
 }
+
+}
+return 0;
+}
+
+int getRdNumber()
+{
+int num= (rand() % 9) + 1;
+return num;
+}
+
+int findMin(int n1, int n2, int n3)
+{
+if (n1 < n2 && n1 < n3)
+return n1;
+if (n2 < n1 && n2 < n3)
+return n2;
+if (n3 < n1 && n3 < n2)
+return n3;
+
+}
+
+int findMax(int n1, int n2, int n3)
+{
+if (n1 > n2 && n1 > n3)
+return n1;
+if (n2 > n1 && n2 > n3)
+return n2;
+if (n3 > n1 && n3 > n2)
+return n3;
+
+}
