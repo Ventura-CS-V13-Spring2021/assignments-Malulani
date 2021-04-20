e259fac78681352848874a3ed7bce443542bb3e6
Malulani Tucker <>
2021-04-20 03:12:31 +0000

    Made a loop to check if the number generated is greater than the previous.

--- Midterm/Question-2.cpp
+++ Midterm/Question-2.cpp
@@ -8,9 +8,19 @@ int isGreater(int num1,int num2);
 
 int main()
 {
-srand(time(0)); //seed for random
+srand(time(0)); 
 ofstream numbers;
-numbers.open("numbers.txt");//opening file
+numbers.open("numbers.txt");
 int N = 10;
-int number=0;//preceding number
-int num[10];//array for tracking numbers
+int number=0;
+int num[10];
+
+for (int i = 1; i < 10; i++)
+{
+num[i] = getNum();
+number = num[i-1];
+if (isGreater(num[i],number))
+{
+numbers << num[i];
+numbers << " ";
+}
