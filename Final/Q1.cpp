a88390b505aa7a0dce797ebaa56532fb34b659ee
Malulani Tucker <>
2021-05-18 15:50:17 +0000

    Divisibles

--- Final/Q1.cpp
+++ Final/Q1.cpp
@@ -8,3 +8,19 @@ int main()
 	const int N=6;
 	int arr[N]={1,2,4,6,10,24};
 	int maxDivisibles=0;
+  int theNum=0;
+	
+	for(int i=0;i<N;i++)
+	{
+		int divisibles=getNumDiv(arr[i]);
+		if(maxDivisibles<=divisibles)
+		{
+      maxDivisibles=divisibles;
+			theNum=arr[i];
+		}
+	}
+	cout<<"The Number with the greatest Number of Divisibles :"<<theNum;
+	return 0;
+}
+
+int getNumDiv(int num)
