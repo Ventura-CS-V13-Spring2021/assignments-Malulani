6dfd97fbcbe3f9a2ab93568b6f55ebd54a3812fb
Malulani Tucker <>
2021-05-18 16:49:32 +0000

    line 8

--- Final/Q2.cpp
+++ Final/Q2.cpp
@@ -5,3 +5,12 @@ class Student {
 	string name;
 	int numClasses;
 	string* classList;
+public:
+	Student() : name(), numClasses(0), classList(NULL) {};
+	
+	Student(string n, int num)
+	{
+		name = n;
+		numClasses = num;
+		classList = new string[num];
+		for (int i = 0; i < numClasses; i++)
