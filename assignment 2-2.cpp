41f602e442b570f75e542232990a468df06feb17
Malulani Tucker <>
2021-05-18 14:42:53 +0000

    2-2

---
+++ assignment 2-2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number1, number2, number3;
+	cout << "Enter three integer value : ";
+	cin >> number1 >> number2 >> number3;
+	if (number1 == number2 && number1 == number3)
+	{
+		cout << "All numbers are same";
+	}
+	else if (number1 == number2 || number1 == number3 || number2 == number3)
+	{
+		cout << "There are two duplicated numbers";
+	}
+	else
+	{
+		cout << "All numbers are distinct";
+	}
+	return 0;
+}
